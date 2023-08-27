837b5738eaebb8855bd2c4f7271d15679e62b04b
Juan Velilla <juandvelillamantilla@gmail.com>
2023-08-27 19:56:58 -0400

    added paths and traveler cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
+import Travelers from './pages/Travelers/Traveler';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-      <Route to="/travelers"/>
+      <Route to="/travelers" element={<Travelers />}/>
       <Route to="/travelers/:id"/>
       <Route to="/trips"/>
       <Route to="/trips/:id"/>
-      <Route to="/trips/:id/new"/>
+      <Route to="/trips/new"/>
       </Routes>
     </BrowserRouter>
   );
