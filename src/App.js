34e91cddfca70a64ba91a4270130ce7f7ee94680
Juan Velilla <juandvelillamantilla@gmail.com>
2023-08-26 20:07:42 -0400

    tryng to add github connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Router, Route } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.scss';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
     <Header />
-    <Router>
+    <Routes>
 
-    </Router>
+    </Routes>
     </BrowserRouter>
   );
 }
