c5e7a9ebd4844867a424ada6af39c3cc2abbc1e9
Juan Velilla <juandvelillamantilla@gmail.com>
2023-09-02 22:22:44 -0400

    finished edit page

--- src/components/AddTrip/AddTrip.js
+++ src/components/AddTrip/AddTrip.js
@@ -1,14 +1,21 @@
-import Destinations from '../Destinations/Destinations'
 import './AddTrip.scss'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+const API_URL = process.env.REACT_APP_API_URL;
+const PORT = process.env.REACT_APP_API_PORT;
 
 function AddTrip() {
+    const [destination, setDestination] = useState(null);
     return (
         <form>
             <input type='text'placeholder='Name of trip' />
             <input type='text'placeholder='Length of Trip' />
             <div>
                 <p>Destinations</p>
-                <Destinations />
+                {destination.map((place) => (
+                    <option key={place.id}>{place.place}</option>
+                ))}
             </div>
             <label>Kids<input type='checkbox' placeholder='Kids Friendly' name="kids" /></label>
             <textarea placeholder='Add travel details'></textarea>
--- src/components/EditTrip/EditTrip.js
+++ src/components/EditTrip/EditTrip.js
@@ -1,44 +1,120 @@
-import Destinations from '../Destinations/Destinations'
 import './EditTrip.scss'
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate} from "react-router-dom";
+import backarrow from "../../assets/images/arrow_back-24px.svg";
+
 const API_URL = process.env.REACT_APP_API_URL;
 const PORT = process.env.REACT_APP_API_PORT;
 
-function AddTrip() {
+function EditTrip() {
     const { id } = useParams();
-    const [tripDetails, setTripDetails] = useState(id);
+    const [destination, setDestination] = useState(null);
+    const [tripDetails, setTripDetails] = useState({
+        trip_name: "",
+        kid_friendly: false,
+        destination_id: "", 
+        trip_length: "",
+        notes: ""
+    });
+    const navigate = useNavigate();
+    const goBack = () => navigate(-1); 
+
+    const [isSubmissionSuccess, setIsSubmissionSuccess] = useState(false);
+
     useEffect(() => {
         axios
             .get(`${API_URL}:${PORT}/trips/${id}`)
             .then((res) => {
                 const tripData = res.data;
-                setTripDetails(tripData);
+                setTripDetails({
+                    ...tripDetails,
+                    trip_name: tripData.trip_name,
+                    kid_friendly: tripData.kid_friendly === "true", 
+                    destination_id: tripData.destination_id,
+                    trip_length: tripData.trip_length,
+                    notes: tripData.notes
+                });
             })
             .catch(error => {
                 console.log(error)
             });
     }, [id])
-    if (!tripDetails) {
+
+    useEffect(() => {
+        axios
+            .get(`${API_URL}:${PORT}/destinations`)
+            .then((res) => {
+                const destinationData = res.data;
+                setDestination(destinationData);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }, []);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const updatedTripDetails = {
+            trip_name: tripDetails.trip_name,
+            kid_friendly: tripDetails.kid_friendly ? "true" : "false",
+            destination_id: tripDetails.destination_id,
+            trip_length: tripDetails.trip_length,
+            notes: tripDetails.notes,
+        };
+
+        axios
+            .put(`${API_URL}:${PORT}/trips/${id}`, updatedTripDetails)
+            .then((res) => {
+                console.log(res.data);
+                setIsSubmissionSuccess(true); console.log(updatedTripDetails)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    if (destination === null) {
         return (
             <>
-                <p>Loading Traveler Data Please Wait.</p>
-            </>)
+                <p>Loading Destination Data Please Wait.</p>
+            </>
+        )
     }
 
     return (
-        <form>
-            <input type='text'placeholder='Name of trip' value={tripDetails.trip_name} />
-            <input type='text'placeholder='Length of Trip' value={tripDetails.trip_length} />
+        <form onSubmit={handleFormSubmit}>
+            <div>
+                <h1>Edit Trip Details</h1>
+                <img src={backarrow} alt="Back arrowkey" onClick={goBack} />
+            </div>
+            <input type='text' placeholder='Name of trip' value={tripDetails.trip_name} onChange={(e) => setTripDetails({ ...tripDetails, trip_name: e.target.value })} />
+            <input type='text' placeholder='Length of Trip' value={tripDetails.trip_length} onChange={(e) => setTripDetails({ ...tripDetails, trip_length: e.target.value })} />
             <div>
                 <p>Destinations</p>
-                <Destinations />
+                <select
+                    value={tripDetails.destination_id}
+                    onChange={(e) => setTripDetails({ ...tripDetails, destination_id: e.target.value })}
+                >
+                    {destination.map((place) => (
+                    <option key={place.id} value={place.id}>{place.place}</option>
+                ))}
+                </select>
             </div>
-            <label>Kids<input type='checkbox' placeholder='Kids Friendly' name="kids" /></label>
-            <textarea placeholder='Add travel details' value={tripDetails.notes}></textarea>
+            <label>
+                Kid Friendly Trip
+                <input
+                    type="checkbox"
+                    placeholder="Kids Friendly"
+                    name="kids"
+                    checked={tripDetails.kid_friendly}
+                    onChange={(e) => setTripDetails({ ...tripDetails, kid_friendly: e.target.checked })}
+                />
+            </label>
+            <textarea placeholder='Add travel details' value={tripDetails.notes} onChange={(e) => setTripDetails({ ...tripDetails, notes: e.target.value })}></textarea>
+            <button type="submit">Update Trip</button>
         </form>
     )
 }
 
-export default AddTrip;
+export default EditTrip;
