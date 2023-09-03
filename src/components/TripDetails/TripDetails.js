7ae5c60b4a7c6c8bf606a4af92fa263ae6831ace
Juan Velilla <juandvelillamantilla@gmail.com>
2023-09-03 00:21:03 -0400

    added post reqest to add trip page

--- src/components/TripDetails/TripDetails.js
+++ src/components/TripDetails/TripDetails.js
@@ -1,8 +1,9 @@
 import "./TripDetails.scss"
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useNavigate} from "react-router-dom"
 import SmallTravelerCard from "../SmallTravelerCard/SmallTraverlerCard";
+import backarrow from "../../assets/images/arrow_back-24px.svg";
 const API_URL = process.env.REACT_APP_API_URL;
 const PORT = process.env.REACT_APP_API_PORT;
 
@@ -10,6 +11,8 @@ function TripDetails() {
     const { id } = useParams();
     const [tripDetails, setTripDetails] = useState(id);
     const travelersForThisTrip = tripDetails.users
+    const navigate = useNavigate();
+    const goBack = () => navigate(-1); 
 
     console.log("travelers", travelersForThisTrip)
 
@@ -34,6 +37,10 @@ function TripDetails() {
     const kidFriendly = tripDetails.kid_friendly === 1 ? "Yes" : "No";
     return (
         <div>
+            <div>
+                <h1>Trip Details</h1>
+                <img src={backarrow} alt="Back arrowkey" onClick={goBack} />
+            </div>
             <p>{tripDetails.trip_name}</p>
             <p>Trip Duration</p>
             <p>{tripDetails.trip_length} days</p>
